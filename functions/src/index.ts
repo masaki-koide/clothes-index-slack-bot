7ef2375455f719e03b86f290ad45e345f9e35e11
masaki.koide <ikasama0324@gmail.com>
2019-01-20 18:28:47 +0900

    feat: Slackのincoming-webhookまで実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import axios from 'axios'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+export const helloWorld = functions.https.onRequest(
+  async (request, response) => {
+    //  response.send("Hello from Firebase!");
+    const res = await axios.post(functions.config().slack.webhook_url, {
+      text: 'Hello from Firebase!'
+    })
+    if (res.status >= 200 && res.status < 300) {
+      const data = res.data
+      response.send(data)
+    } else {
+      response.send(res.statusText)
+    }
+  }
+)
